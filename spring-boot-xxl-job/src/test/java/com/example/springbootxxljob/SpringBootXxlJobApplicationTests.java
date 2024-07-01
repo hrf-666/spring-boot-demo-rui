5a985b219f9f6a0604cb9ff145d7f0360ac73ae6
hrf <3398775460@qq.com>
2024-07-01 13:49:19 +0800

    提交

---
+++ spring-boot-xxl-job/src/test/java/com/example/springbootxxljob/SpringBootXxlJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootxxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXxlJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
