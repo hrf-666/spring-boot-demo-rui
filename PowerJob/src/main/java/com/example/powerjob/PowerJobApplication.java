2700a67dab7b669e4e160e00a6ecbe4e6b0ea6a7
hrf <3398775460@qq.com>
2024-07-02 08:49:22 +0800

    Your commit message

---
+++ PowerJob/src/main/java/com/example/powerjob/PowerJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerJobApplication.class, args);
+    }
+
+}
